1cd6e3cc68cb4712429b743a7ac4306c5212445e
Garry Kuwanto <gkuwanto@gmail.com>
2020-06-18 22:00:37 +0700

    Updates

---
+++ precache-manifest.a7aa97fea4614b6324a4479331841ef5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fe62aa0a2c86fd6db277d9ded7920f",
+    "url": "/fourier/index.html"
+  },
+  {
+    "revision": "3f4ef1d2b453727086bb",
+    "url": "/fourier/static/css/main.21daed7c.chunk.css"
+  },
+  {
+    "revision": "74a8c211bc7433677e76",
+    "url": "/fourier/static/js/2.5d90ef59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fourier/static/js/2.5d90ef59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f4ef1d2b453727086bb",
+    "url": "/fourier/static/js/main.072c7b10.chunk.js"
+  },
+  {
+    "revision": "dd91f2ed8a858610450e",
+    "url": "/fourier/static/js/runtime-main.9a2e0ced.js"
+  },
+  {
+    "revision": "1540cda5d3d7e122c9beea354a2de5de",
+    "url": "/fourier/static/media/Fourier Vector.1540cda5.svg"
+  },
+  {
+    "revision": "ad8d068b714c4beeed688110ac5ab39e",
+    "url": "/fourier/static/media/background.ad8d068b.jpg"
+  }
+]);
